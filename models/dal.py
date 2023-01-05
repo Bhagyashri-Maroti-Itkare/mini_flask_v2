05c0292a32a830c32d146f1243b849b25eead27a
Bhagyashri-Maroti-Itkare <itkareb@gmail.com>
2023-01-05 12:15:41 +0530

    mini flask v2

---
+++ models/dal.py
@@ -0,0 +1,15 @@
+"""
+DAL : Data Access Layer
+This module contains code to retrieve and store data (data manipulation)
+"""
+
+import json
+
+
+def load_db():
+    file_path = "C:\\Users\shri\PycharmProjects\\mini_flask_v2\models\\flashcard_db.json"
+    with open(file_path, "r") as foo:
+        return json.load(foo)
+
+
+db = load_db()
